47600e0a78ceb53779f347da5bd5b71a698d2414
cher0z <reni.rasa@gmail.com>
2019-04-11 20:47:12 +0200

    created a class to be used for checking input pattern

--- src/main/java/com/renaldorasa/fastcodetest/PatternChecker.java
+++ src/main/java/com/renaldorasa/fastcodetest/PatternChecker.java
@@ -3,9 +3,17 @@ package com.renaldorasa.fastcodetest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+public class PatternChecker {
 
-public class ValueScrambler {
+    private Pattern valuePattern = Pattern.compile("^(([1-9][0-9]*)|0)p(([1-9][0-9]*)|0)s(([1-9][0-9]*)|0)$");
+    private Matcher m;
 
-    Pattern valuePattern = Pattern.compile("(^[1-9][0-9]*)p([1-9][0-9]*)s([1-9][0-9]*)");
-    Matcher matcher;
+    // Check if the input value correspongs to the format #p#s#d
+
+    boolean checkPattern(String text){
+
+        m = valuePattern.matcher(text);
+
+        return m.matches();
+    }
 }
